241612a76c78ad74e25a260bda1f299753373c94
Mahlori412 <manganyimahlori6@gmail.com>
2022-08-11 00:51:02 +0200

    still fixing

--- main.c
+++ main.c
@@ -1,73 +1,51 @@
-#include "shell.h"
-/**
-* signal_handler - handles CTRL+C, doesn't exit
-* @sig : value to change
-* Description: handles CTRL+C, doesn't exit
-* Return: nada
-*/
-void signal_handler(int sig)
-{
-(void)sig;
-write(STDOUT_FILENO, "\n$ ", 3);
-}
+#include "file.h"
 
 /**
- * prompt - checks mode and prints prompt if in interactive mode
- * @fd: file stream
- * @buf: buffer
-**/
-void prompt(int fd, struct stat buf)
+ * main - Main function
+ * @argc: Number of arguments
+ * @argv: List of arguments
+ * @envp: List of enviroments
+ * Return: 0
+ */
+int main(int argc, char **argv, char *envp[])
 {
-	fstat(fd, &buf);
-	if (S_ISCHR(buf.st_mode))
-	write(STDOUT_FILENO, "$ ", 2);
-}
+	vars_t vars = {NULL, NULL};
+	size_t len_buffer = 0;
+	int i;
 
-/**
-* main - entry point, main shell loop
-* @void : nada
-* Description: main shell loop
-* Return: 0 for success
-*/
-int main(void)
-{
-char *line, *path, *fullpath, **tokens;
-int alert, builtin_stat, child_stat;
-struct stat buf;
-signal(SIGINT, signal_handler); /*SIGINT ignores interupt signal (ctrl+C) */
-while (1) /* True, infinite loop */
-{
-prompt(STDIN_FILENO, buf);/*STDIN_FILENO MACRO== file descriptor == 0*/
-line = _getline(stdin);/*FILE * line = input cmd*/
-if (_strcmp(line, "\n", 1) == 0) /*if input equals \n */
-{
-free(line);
-continue;
-}
-tokens = parse(line);/*Parse input taking out delims, tokens = input cmd*/
-if (tokens[0] == NULL)
-continue;
-builtin_stat = exec_builtin(tokens); /*search through builtin*/
-if (builtin_stat == 0 || builtin_stat < 0)
-{
-free(tokens);
-free(line);
-}
-if (builtin_stat == 0) /* is env builtin */
-continue;
-if (builtin_stat == -1) /* is exit builtin */
-exit(EXIT_SUCCESS);
-alert = 0; /* 0 if fullpath isn't malloc*/
-path = _getenv("PATH");
-fullpath = search_for_path(tokens[0], fullpath, path); /* gets pathname */
-if (fullpath == NULL)
-fullpath = tokens[0];
-else
-alert = 1; /* 1 if fullpath is malloc*/
-child_stat = child(fullpath, tokens);
-if (child_stat == -1)
-perror("./hsh: ");
-free_everything(tokens, path, line, fullpath, alert); /*free everythiSng */
-}
-return (0);
+	(void)argc;
+	(void)argv;
+
+	_puts("$ ");
+
+	while ((getline(&(vars.buffer), &len_buffer, stdin)) != -1)
+	{
+
+		if (!strcmp(vars.buffer, "env\n"))
+		{
+			for (i = 0 ; envp[i] ; i++)
+			{
+				_puts(envp[i]);
+				_putchar('\n');
+			}
+		}
+
+		vars.array_tokens = tokenizer(vars.buffer, " \n");
+		if (vars.array_tokens == NULL)
+		{
+			_puts("$ ");
+			continue;
+		}
+
+		if (check_for_builtins(&vars) == NULL)
+		{
+			_puts("No encontrado ");
+			_puts(vars.array_tokens[0]);
+			_putchar('\n');
+		}
+		free_shell(&vars);
+		_puts("$ ");
+	}
+	_putchar('\n');
+	exit(0);
 }
