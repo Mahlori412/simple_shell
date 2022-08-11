768dfb732d3ca4a06a349d406dc29a0d6e1c57fd
Mahlori412 <manganyimahlori6@gmail.com>
2022-08-11 19:34:06 +0200

    again

--- shell.h
+++ shell.h
@@ -1,114 +1,101 @@
 #ifndef SHELL_H
 #define SHELL_H
-
-/* header files */
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <errno.h>
-
-/* Global variable */
-extern char **environ;
-
-/* Macros */
-#define BUFSIZE 256
-#define TOKENSIZE 64
-#define PRINT(c) (write(STDOUT_FILENO, c, _strlen(c)))
+#include <stdarg.h>
+#include <stdint.h>
+#define BUFF_SIZE 1024
 #define PROMPT "$ "
-#define SUCCESS (1)
-#define FAIL (-1)
-#define NEUTRAL (0)
-
-/* Struct */
-
+/* error messages */
+#define EINVAL "Invalid argument"
+#define ENOMEM "Out of memory"
+#define ERROR "Error"
+#define WRONG "Something went wrong"
+#define END "exit"
 /**
- * struct sh_data - Global data structure
- * @line: the line input
- * @args: the arguments token
- * @error_msg: the global path
- * @cmd: the parsed command
- * @index: the command index
- * @oldpwd: the old path visited
- * @env: the environnment
- *
- * Description: A structure contains all the variables needed to manage
- * the program, memory and accessability
+ * struct built_in - list of builtins
+ * @bi: The specifier
+ * @f: The function associated with printing
  */
-typedef struct sh_data
+typedef struct built_in
 {
-	char *line;
-	char **args;
-	char *cmd;
-	char *error_msg;
-	char *oldpwd;
-	unsigned long int index;
-	char *env;
-} sh_t;
+	char *bi;
+	int (*f)();
+} builtin_t;
 /**
- * struct builtin - Manage the builtin functions
- * @cmd: the command line on string form
- * @f: the associated function
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: this struct made to manage builtins cmd
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-typedef struct builtin
+typedef struct list_s
 {
-	char *cmd;
-	int (*f)(sh_t *data);
-} blt_t;
-/* ----------Process prototype------------*/
-int read_line(sh_t *);
-int split_line(sh_t *);
-int parse_line(sh_t *);
-int process_cmd(sh_t *);
-
-/* ----------String prototype------------*/
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+extern char **environ;
+/* lists.c */
+size_t list_len(list_t *h);
+list_t *add_node(list_t **head, char *str);
+list_t *add_node_end(list_t **head, char *str);
+void free_list(list_t *head);
+list_t *get_node(list_t **head, char *str);
+/* prints.c */
+int _putchar(char c);
+void _strprint(char *str);
+void print_array(char **array);
+size_t print_list(list_t *h);
+/* strings.c */
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
 char *_strdup(char *str);
-char *_strcat(char *first, char *second);
-int _strlen(char *str);
-char *_strchr(char *str, char c);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+/* more_strings */
 int _strcmp(char *s1, char *s2);
-
-/* ----------More String prototype-------*/
-char *_strcpy(char *dest, char *source);
-
-/* ----------Memory prototype------------*/
+char *_strchr(char *str, char c);
+int len_to_char(char *str, char c);
+int _atoi(char *s);
+/* _strtok.c */
+int count_words(char *str, char delim);
+int _wrdlen(char *s, char delim);
+char **strtow(char *str, char delim);
+/*  array_list.c */
+int arr_size(char **arr);
+list_t *array_to_list(char **array);
+char **list_to_array(list_t **head);
+void free_array(char **array);
+/* new_env.c */
+char *_getenv(char *name, list_t **env_head);
+int _setenv(char *name, char *value, list_t **env_head);
+int _unsetenv(char *name, list_t **env_head);
+int delete_node(list_t **head, char *string);
+char *var_str(char *name, char *value);
+/* _getline.c */
+int _getline(char *input, int size);
+int exit_shell(char **line_tok);
+void clear_buffer(char *buffer);
+char *mem_cpy(char *dest, char *src, int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memset(char *s, char byt, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int free_data(sh_t *);
-
-/* ----------Tools prototype-------------*/
-void *fill_an_array(void *a, int el, unsigned int len);
-void signal_handler(int signo);
-char *_getenv(char *path_name);
-void index_cmd(sh_t *data);
-void array_rev(char *arr, int len);
-
-/* ----------More tools prototype--------*/
-char *_itoa(unsigned int n);
-int intlen(int num);
-int _atoi(char *c);
-int print_error(sh_t *data);
-int write_history(sh_t *data);
-int _isalpha(int c);
-
-/* -------------Builtins-----------------*/
-int abort_prg(sh_t *data);
-int change_dir(sh_t *data);
-int display_help(sh_t *data);
-int handle_builtin(sh_t *data);
-int check_builtin(sh_t *data);
-
-/* -------------Parse-----------------*/
-int is_path_form(sh_t *data);
-void is_short_form(sh_t *data);
-int is_builtin(sh_t *data);
-
-#endif /* SHELL_H */
+/* cmd_line_loop.c */
+int cmd_line_loop(char *buffer, char *line, list_t **env_head);
+/* run_command */
+char **path_dirs_array(list_t **env_head);
+char *cmd_in_path(char *str, list_t **env_head);
+int run_command(char **line, list_t **env_head);
+/* built_ins.c */
+int built_ins(char **input, list_t **env_head);
+int exit_bi(char **line);
+int print_env(char **line, list_t **env_head);
+int set_env(char **line, list_t **env_head);
+int unset_env(char **line, list_t **env_head);
+#endif
